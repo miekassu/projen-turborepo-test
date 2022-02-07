ab6475a47c8d79bb73273bc93c62c6708c600930
Roman Filippov <rf@romanfilippov.com>
2022-02-07 16:32:04 +0700

    style: semi off

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,21 @@
-import { TurborepoProject } from "projen-turborepo";
+import { TurborepoProject } from 'projen-turborepo'
+
 const project = new TurborepoProject({
-  defaultReleaseBranch: "main",
-  devDeps: ["projen-turborepo@0.0.38"],
-  name: "projen-turborepo-test",
+  defaultReleaseBranch: 'main',
+  devDeps: ['projen-turborepo@0.0.38'],
+  name: 'projen-turborepo-test',
   projenrcTs: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
-});
-project.synth();
+})
+
+project.eslint?.addRules({
+  semi: [
+    'error',
+    'never',
+  ],
+})
+
+project.synth()
